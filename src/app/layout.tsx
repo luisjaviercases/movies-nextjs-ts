efb82bb91c38b060d2a8788130a569fc35022da3
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-06 18:51:12 +0100

    feat: add ESLint and prettier configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,21 @@
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
 
 const roboto = Roboto({
   subsets: ['latin'],
-  weight: ["400", "500", "700", "900"],
-})
+  weight: ['400', '500', '700', '900'],
+});
 
 export const metadata: Metadata = {
   title: 'NextJS app',
   description: 'Make API requests with JWT',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={roboto.className}>{children}</body>
     </html>
-  )
+  );
 }
