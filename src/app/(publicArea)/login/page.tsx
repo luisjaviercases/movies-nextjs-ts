c13abb5276a13f25512e07e271e62bf20a081848
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-07 15:42:52 +0100

    feat(login): adapt page to new styles

--- src/app/(publicArea)/login/page.tsx
+++ src/app/(publicArea)/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from './styles.module.scss';
 import BackgroundImage from '@/components/BackgroundImage/BackgroundImage';
 import LoginForm from '@/components/LoginForm/LoginForm';
 
@@ -11,7 +12,7 @@ export default function Login() {
   return (
     <main>
       <BackgroundImage imageName='login_background.png' />
-      <div>
+      <div className={styles.container}>
         <LoginForm onSignIn={onSignIn} />
       </div>
     </main>
