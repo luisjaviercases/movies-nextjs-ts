5c70c2e703feb081a2987c9a23f3eee8028ec898
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-07 15:41:45 +0100

    feat(login): add LoginForm to page

--- src/app/(publicArea)/login/page.tsx
+++ src/app/(publicArea)/login/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import BackgroundImage from '@/components/BackgroundImage/BackgroundImage';
+import LoginForm from '@/components/LoginForm/LoginForm';
 
 export default function Login() {
+  const onSignIn = async (values: { username: string; password: string }) => {
+    console.log('onSignIn', values);
+  };
+
   return (
     <main>
       <BackgroundImage imageName='login_background.png' />
+      <div>
+        <LoginForm onSignIn={onSignIn} />
+      </div>
     </main>
   );
 }
