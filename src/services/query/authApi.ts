505456e125320c13ff625b6800458794b86b5df1
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-07 16:56:17 +0100

    feat(auth-api): create api with requests to make login

---
+++ src/services/query/authApi.ts
@@ -0,0 +1,17 @@
+import baseApi from '@/services/query/baseApi';
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    authSignIn: builder.mutation<{ token: string }, { email: string; password: string }>({
+      query: (body) => {
+        return {
+          url: '/films/auth/sign-in',
+          method: 'post',
+          body,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useAuthSignInMutation } = authApi;
