b17c6ae67ab10051ae133352bba5bc73e529d7a1
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-07 16:56:46 +0100

    feat(store): add auth reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { authApi } from '@/services/query/authApi';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ [authApi.reducerPath]: authApi.reducer });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([]),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
