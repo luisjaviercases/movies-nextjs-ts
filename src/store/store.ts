6d05e1c3dbf443f785d21bb2a7803d91ea04a80d
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-08 11:38:53 +0100

    feat(store): configure store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { authApi } from '@/services/query/authApi';
+import { BASE_API_REDUCER_KEY, baseApi } from '@/services/query/baseApi';
 
-const rootReducer = combineReducers({ [authApi.reducerPath]: authApi.reducer });
+const rootReducer = combineReducers({ [BASE_API_REDUCER_KEY]: baseApi.reducer });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApi.middleware]),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([baseApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
