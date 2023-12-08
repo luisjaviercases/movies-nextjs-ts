e88a50c00c6a070e257b2818d81d4d11d872858f
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-08 11:40:57 +0100

    feat(models): create Movie interface

---
+++ src/models/movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  id: string;
+  title: string;
+  description: string;
+  genre: string;
+  availableDate: string;
+  thumbnail: string;
+  cast: string;
+  poster: string;
+  rating: number;
+  highlighted: boolean;
+}
