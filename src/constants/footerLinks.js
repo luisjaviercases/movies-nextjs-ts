6510a184ce13ee73f13d2ab601ae26fd2064797d
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-09 10:13:09 +0100

    feat(constants): add footer links

---
+++ src/constants/footerLinks.js
@@ -0,0 +1,14 @@
+export const footerLinks = [
+  [
+    { text: 'Link 1', url: '#' },
+    { text: 'Link 2', url: '#' },
+    { text: 'Link 3', url: '#' },
+    { text: 'Link 4', url: '#' },
+  ],
+  [
+    { text: 'Link 5', url: '#' },
+    { text: 'Link 6', url: '#' },
+  ],
+  [{ text: 'Link 7', url: '#' }],
+  [{ text: 'Link 8', url: '#' }],
+];
