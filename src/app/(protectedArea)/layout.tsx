826dc33b2c042143915a990dec531544cc13f591
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-09 10:18:03 +0100

    feat(layout): add footer and styles to protected area layout

--- src/app/(protectedArea)/layout.tsx
+++ src/app/(protectedArea)/layout.tsx
@@ -1,9 +1,17 @@
+import Footer from '@/components/Footer/Footer';
 import { MovieProvider } from '@/context/MovieContext';
+import { footerLinks } from '@/constants/footerLinks';
+import styles from './layout.module.scss';
 
 export default function ProtectedAreaLayout({ children }: { children: React.ReactNode }) {
   return (
     <main>
-      <MovieProvider>{children} </MovieProvider>
+      <MovieProvider>
+        <div className={styles.container}>
+          {children}
+          <Footer links={footerLinks} />
+        </div>
+      </MovieProvider>
     </main>
   );
 }
