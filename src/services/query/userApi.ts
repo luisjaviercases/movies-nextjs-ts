c0d0cfb72d5e6d7199d8adac27779175b17ad481
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-10 21:29:33 +0100

    feat(user-api): create api and add endpoints

---
+++ src/services/query/userApi.ts
@@ -0,0 +1,32 @@
+import { baseApi } from '@/services/query/baseApi';
+
+export const userApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    addMovieToUserList: builder.mutation<{ myList: string[] }, { id: string }>({
+      query: (body) => {
+        return {
+          url: '/films/user/list',
+          method: 'POST',
+          body,
+        };
+      },
+    }),
+    removeMovieFromUserList: builder.mutation<{ myList: string[] }, string>({
+      query: (id) => {
+        return {
+          url: `/films/user/list/${id}`,
+          method: 'DELETE',
+        };
+      },
+    }),
+    getUserMoviesList: builder.query<string[], void>({
+      query: () => ({
+        url: '/films/user',
+        method: 'GET',
+      }),
+      keepUnusedDataFor: 0,
+    }),
+  }),
+});
+
+export const { useAddMovieToUserListMutation, useRemoveMovieFromUserListMutation, useGetUserMoviesListQuery } = userApi;
