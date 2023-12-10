f1eaf56fea1e5a4713ffd24f6e7a229b24f4c55d
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-10 12:10:13 +0100

    feat(layout): add signout to protected area layout

--- src/app/(protectedArea)/layout.tsx
+++ src/app/(protectedArea)/layout.tsx
@@ -2,12 +2,14 @@ import Footer from '@/components/Footer/Footer';
 import { MovieProvider } from '@/context/MovieContext';
 import { footerLinks } from '@/constants/footerLinks';
 import styles from './layout.module.scss';
+import SignOut from '@/components/SignOut/SignOut';
 
 export default function ProtectedAreaLayout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <MovieProvider>
         <div className={styles.container}>
+          <SignOut />
           {children}
           <Footer links={footerLinks} />
         </div>
