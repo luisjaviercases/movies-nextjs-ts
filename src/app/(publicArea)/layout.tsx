efc3f5b7cd16384b3de8d4790cfb8429d08b2b7e
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-11 19:40:31 +0100

    refactor(layout): changes in public-area layout

--- src/app/(publicArea)/layout.tsx
+++ src/app/(publicArea)/layout.tsx
@@ -1,7 +1,3 @@
 export default function PublicAreaLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <main>{children}Public layout</main>
-    </div>
-  );
+  return <main>{children}</main>;
 }
