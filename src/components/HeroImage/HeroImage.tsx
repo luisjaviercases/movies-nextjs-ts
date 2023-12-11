5a99d39e3d359f86a9777b4708e977ea9481def6
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-11 20:17:13 +0100

    feat(hero-image): add component to show image in hero
    Also add styles

---
+++ src/components/HeroImage/HeroImage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import { FC } from 'react';
+import styles from './HeroImage.module.scss';
+
+interface HeroImageProps {
+  src: string;
+  alt: string;
+}
+
+const HeroImage: FC<HeroImageProps> = ({ src, alt }) => {
+  return (
+    <div className={styles['container']}>
+      <Image className={styles['container__image']} priority fill src={src} alt={alt} />
+    </div>
+  );
+};
+
+export default HeroImage;
