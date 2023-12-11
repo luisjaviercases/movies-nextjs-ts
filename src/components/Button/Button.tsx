9d66330c9a015f80ac285ed75bf48fd5580e0fb7
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-11 15:45:06 +0100

    feat(button): modify component to add variants
    Also add styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,14 @@ import { FC, ReactNode, MouseEvent, ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   children: ReactNode;
 }
 
-const Button: FC<ButtonProps> = ({ onClick, children, ...rest }) => {
+const Button: FC<ButtonProps> = ({ onClick, children, variant = 'primary', ...rest }) => {
   return (
-    <button className={styles.button} onClick={onClick} {...rest}>
+    <button className={`${styles.button} ${styles[`button--${variant}`]}`} onClick={onClick} {...rest}>
       {children}
     </button>
   );
