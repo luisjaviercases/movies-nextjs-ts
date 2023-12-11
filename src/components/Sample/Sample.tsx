ea644b72ff570ebdc35ecaa35952fbb4fddf265e
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-11 16:09:12 +0100

    refactor: remove sample component

--- src/components/Sample/Sample.tsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react';
-import styles from './Sample.module.scss';
-
-const Sample: React.FC = () => {
-  return <span className={styles.sample}>Sample component</span>;
-};
-
-export default memo(Sample);
