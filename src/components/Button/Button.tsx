b0bcfe89f87f5c33a13a4a3bb7f0a7eb4c92caa2
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-12 16:59:37 +0100

    feat(button): add size prop, small by default
    Also add styles for big size

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,14 +2,18 @@ import { FC, ReactNode, MouseEvent, ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.scss';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'small' | 'big';
   variant?: 'primary' | 'secondary';
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   children: ReactNode;
 }
 
-const Button: FC<ButtonProps> = ({ onClick, children, variant = 'primary', ...rest }) => {
+const Button: FC<ButtonProps> = ({ onClick, children, variant = 'primary', size = 'small', ...rest }) => {
   return (
-    <button className={`${styles.button} ${styles[`button--${variant}`]}`} onClick={onClick} {...rest}>
+    <button
+      className={`${styles.button} ${styles[`button--${variant}`]} ${styles[`button--${size}`]}`}
+      onClick={onClick}
+      {...rest}>
       {children}
     </button>
   );
