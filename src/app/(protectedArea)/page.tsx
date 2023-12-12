d0ba5f62e42e9bb5ef8996017ea1d2a3b4a79ea1
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-12 09:43:09 +0100

    feat(protected-area): add content to main page

--- src/app/(protectedArea)/page.tsx
+++ src/app/(protectedArea)/page.tsx
@@ -1,30 +1,73 @@
 'use client';
 
-import Sample from '@/components/Sample/Sample';
-import { useMovieContext } from '@/context/MovieContext';
+import { useMemo, useState } from 'react';
+import Button from '@/components/Button/Button';
+import HeroSlider from '@/components/HeroSlider/HeroSlider';
+import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
+import PageContainer from '@/components/PageContainer/PageContainer';
+import { Movie } from '@/models/movie';
+import { useGetGenresQuery } from '@/services/query/genresApi';
 import { useGetMoviesQuery } from '@/services/query/moviesApi';
-import Link from 'next/link';
+import styles from './page.module.scss';
+import Carousel from '@/components/Carousel/Carousel';
 
 export default function Home() {
-  const { data: movies } = useGetMoviesQuery();
-  const { setMovieContext } = useMovieContext();
+  const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
+  const { data: movies, isLoading: isLoadingMovies } = useGetMoviesQuery();
+  const { data: genres, isLoading: isLoadingGenres } = useGetGenresQuery();
 
-  console.log('Movies', movies);
+  const moviesByGenre: Record<string, Movie[]> = {};
 
-  const handleMovieClick = (movieId: number) => {
-    setMovieContext(movieId);
+  movies?.forEach((movie) => {
+    const genreId = movie.genre;
+    if (!moviesByGenre[genreId]) {
+      moviesByGenre[genreId] = [];
+    }
+    moviesByGenre[genreId].push(movie);
+  });
+
+  const highlightedMovies = useMemo(() => {
+    return (movies || []).filter((movie) => movie.highlighted);
+  }, [movies]);
+
+  const handleGenreClick = (genreId: string) => {
+    setSelectedGenre(selectedGenre === genreId ? null : genreId);
   };
 
   return (
     <>
-      <h1>Home page</h1>
-      <Link href={`/movies/${'Movie title 1'}`} onClick={() => handleMovieClick(1000)}>
-        Movie details 1
-      </Link>
-      <Link href={`/movies/${'Movie title 2'}`} onClick={() => handleMovieClick(1001)}>
-        Movie details 2
-      </Link>
-      <Sample />
+      {isLoadingMovies || isLoadingGenres ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <HeroSlider movies={highlightedMovies} />
+          <PageContainer>
+            <div className={styles['filter-buttons']}>
+              {genres?.map((genre) => (
+                <div key={genre.id} className={styles['filter-buttons__button-container']}>
+                  <Button
+                    variant={genre.id === selectedGenre ? 'primary' : 'secondary'}
+                    onClick={() => handleGenreClick(genre.id)}>
+                    {genre.name}
+                  </Button>
+                </div>
+              ))}
+            </div>
+            <div className={styles['carousel-list']}>
+              {genres?.map((genre) => (
+                <>
+                  {selectedGenre === null || selectedGenre === genre.id ? (
+                    <div key={genre.id} className={styles['carousel-list__container']}>
+                      <h2 className={styles['carousel-list__container__title']}>{genre.name}</h2>
+                      <Carousel movies={moviesByGenre[genre.id]} />
+                    </div>
+                  ) : null}
+                </>
+              ))}
+            </div>
+          </PageContainer>
+        </>
+      )}
     </>
   );
 }
