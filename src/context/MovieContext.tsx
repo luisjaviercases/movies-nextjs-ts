8092130fce818323b243ac04034348c3f035c023
Luis Javier Cases <l.cases@cathedralsw.com>
2023-12-12 15:56:32 +0100

    feat(context): remove unnecessary state

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -4,9 +4,7 @@ import { createContext, useContext, useState, ReactNode } from 'react';
 
 interface MovieContextType {
   movieId: string | null;
-  listOfMovieIds: string[];
   setMovieIdContext: (id: string) => void;
-  setListOfMovieIdsContext: (listOfMovieIds: string[]) => void;
 }
 
 const MovieContext = createContext<MovieContextType | undefined>(undefined);
@@ -17,21 +15,12 @@ interface MovieProviderProps {
 
 export const MovieProvider: React.FC<MovieProviderProps> = ({ children }) => {
   const [movieId, setMovieId] = useState<string | null>(null);
-  const [listOfMovieIds, setListOfMovieIds] = useState<string[]>([]);
 
   const setMovieIdContext = (id: string | null) => {
     setMovieId(id);
   };
 
-  const setListOfMovieIdsContext = (listOfMovieIds: string[]) => {
-    setListOfMovieIds(listOfMovieIds);
-  };
-
-  return (
-    <MovieContext.Provider value={{ movieId, listOfMovieIds, setMovieIdContext, setListOfMovieIdsContext }}>
-      {children}
-    </MovieContext.Provider>
-  );
+  return <MovieContext.Provider value={{ movieId, setMovieIdContext }}>{children}</MovieContext.Provider>;
 };
 
 export const useMovieContext = (): MovieContextType => {
