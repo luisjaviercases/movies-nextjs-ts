61b8f10714f0b110975106832bae7f4ab563a9f0
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-14 14:45:54 +0100

    feat(sign-out): remove signout request
    Only remove token when user do clic in component

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -4,16 +4,13 @@ import { FC, useState, useRef, KeyboardEvent } from 'react';
 import Image from 'next/image';
 import Button from '@/components/Button/Button';
 import styles from './SignOut.module.scss';
-import { useLazyAuthSignOutQuery } from '@/services/query/authApi';
 import { removeCookie } from '@/app/actions';
 import { useRouter } from 'next/navigation';
-import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
 
 const SignOut: FC = () => {
   const router = useRouter();
   const [isButtonVisible, setButtonVisible] = useState<boolean>(false);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const [signOut, { isLoading }] = useLazyAuthSignOutQuery();
 
   const handleIconHover = () => {
     if (timeoutRef.current) {
@@ -30,7 +27,6 @@ const SignOut: FC = () => {
 
   const handleButtonClick = async () => {
     try {
-      await signOut();
       await removeCookie('userToken');
       router.push('/login');
       window.history.replaceState(null, '', '/login');
@@ -46,29 +42,23 @@ const SignOut: FC = () => {
   };
 
   return (
-    <>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className={styles.container} data-testid='signout'>
-          <button
-            className={styles['container__icon']}
-            onMouseEnter={handleIconHover}
-            onMouseLeave={handleIconLeave}
-            onKeyDown={handleKeyDown}
-            data-testid='signout--icon-button'>
-            <Image src={`/icons/icon-user.svg`} alt='Icon user' width='38' height='38' />
-          </button>
-          {isButtonVisible && (
-            <div className={styles['container__custom-button']}>
-              <Button onMouseEnter={handleIconHover} onMouseLeave={handleIconLeave} onClick={handleButtonClick}>
-                Sign out
-              </Button>
-            </div>
-          )}
+    <div className={styles.container} data-testid='signout'>
+      <button
+        className={styles['container__icon']}
+        onMouseEnter={handleIconHover}
+        onMouseLeave={handleIconLeave}
+        onKeyDown={handleKeyDown}
+        data-testid='signout--icon-button'>
+        <Image src={`/icons/icon-user.svg`} alt='Icon user' width='38' height='38' />
+      </button>
+      {isButtonVisible && (
+        <div className={styles['container__custom-button']}>
+          <Button onMouseEnter={handleIconHover} onMouseLeave={handleIconLeave} onClick={handleButtonClick}>
+            Sign out
+          </Button>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
